dd251ae395f01dabd9743230a448616f61a4a103
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2026-05-09 15:20:30 +0300

    git

--- lab_03/src/server.ts
+++ lab_03/src/server.ts
@@ -1,29 +1,26 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 import app from './app';
 import { connectDB } from './config/database';
 import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+const HOST = '0.0.0.0';
 
 async function startServer() {
     await connectDB();
 
-    const server = app.listen(PORT, () => {
-        console.log(`Сервер успішно запущено на порту ${PORT}`);
+    const server = app.listen(PORT, HOST, () => {
+        console.log(`🚀 Server running on http://${HOST}:${PORT}`);
     });
 
     process.on('SIGTERM', gracefulShutdown);
     process.on('SIGINT', gracefulShutdown);
 
     function gracefulShutdown(signal: string) {
-        console.log(`\nОтримано ${signal}. Грасійне завершення роботи...`);
         server.close(async () => {
-            console.log('HTTP сервер зупинено.');
             await mongoose.connection.close();
-            console.log('Підключення до MongoDB закрито.');
             process.exit(0);
         });
     }
